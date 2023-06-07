ebf41be439d631ff4bad52a34207670aee355d7e
Yiqiao Yin <eagle0504@gmail.com>
2023-06-07 18:59:50 -0400

    new files

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# streamlit-chatgpt-ui
-A minimal ChatGPT-like UI built with Streamlit
+# WYN-Chat-IO
+
+WYN-Chat-IO is a repository that uses the Streamlit package to build a chatbot. The streamlit package helps in designing the frontend, whereas the streamlit_chat package is used to design the chat history of the bot. The main objective of this project is to create an interactive chatbot that can communicate with users and provide them assistance with various tasks.
+
+## Requirements
+
+To run the WYN-Chat-IO repository, you need to have Python 3.7 or later installed on your system. You also need to install the following packages:
+
+- streamlit
+- streamlit_chat
+
+You can install these packages using pip by running the following command:
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+To use the WYN-Chat-IO repository, clone the repository to your local machine using the following command:
+
+```
+git clone https://github.com/user/WYN-Chat-IO.git
+```
+
+After cloning the repository, navigate to the directory and run the following command:
+
+```
+streamlit run app.py
+```
+
+This will start the Streamlit application, and you will be able to interact with the chatbot.
+
+## Features
+
+The WYN-Chat-IO repository offers the following features:
+
+- Interactive chatbot that communicates with users and provides assistance.
+- Easy-to-use interface designed using the Streamlit package.
+- Chat history designed using the streamlit_chat package.
+
+## Contribution
+
+If you find any bugs or issues with the WYN-Chat-IO repository, feel free to create an issue or submit a pull request. We welcome contributions from the community.
+
+## Conclusion
+
+The WYN-Chat-IO repository offers an excellent solution for building an interactive chatbot using the Streamlit package. It provides an easy-to-use interface and a chat history designed using the streamlit_chat package. With its advanced features and ease of use, it is a great tool for developers looking to create an interactive chatbot.
--- app.py
+++ app.py
@@ -1,77 +1,292 @@
+import json
+from typing import Dict, List, Union
+
+import numpy as np
 import openai
+import os
+import pandas as pd
 import streamlit as st
+import google.generativeai as palm
+from scipy.spatial.distance import cosine
+
+from sentence_transformers import SentenceTransformer
 from streamlit_chat import message
 
 # Setting page title and header
-st.set_page_config(page_title="AVA", page_icon=":robot_face:")
-st.markdown("<h1 style='text-align: center;'>AVA - a totally harmless chatbot 😬</h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="WYN AI", page_icon=":robot_face:")
+st.markdown(
+    f"""
+        <h1 style='text-align: center;'>W.Y.N. Artificial Intelligence😬</h1>
+    """,
+    unsafe_allow_html=True,
+)
 
-# Set org ID and API key
-openai.organization = "<YOUR_OPENAI_ORG_ID>"
-openai.api_key = "<YOUR_OPENAI_API_KEY>"
+# Load data
+df = pd.read_csv("lh_ar_2022.csv")
 
 # Initialise session state variables
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
         {"role": "system", "content": "You are a helpful assistant."}
     ]
-if 'model_name' not in st.session_state:
-    st.session_state['model_name'] = []
-if 'cost' not in st.session_state:
-    st.session_state['cost'] = []
-if 'total_tokens' not in st.session_state:
-    st.session_state['total_tokens'] = []
-if 'total_cost' not in st.session_state:
-    st.session_state['total_cost'] = 0.0
+if "domain_name" not in st.session_state:
+    st.session_state["domain_name"] = []
+
 
 # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
 st.sidebar.title("Sidebar")
-model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+model_name = st.sidebar.radio(
+    "Choose a model:", ("ChatGPT", "Palm", "Next...")
+)
+domain_name = st.sidebar.radio(
+    "Choose a domain:", ("General", "Labcorp 2022 Annual Report", "Upload Your Own")
+)
 counter_placeholder = st.sidebar.empty()
-counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+counter_placeholder.write(f"Next item ... ")
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
-
-# Map model names to OpenAI model IDs
-if model_name == "GPT-3.5":
-    model = "gpt-3.5-turbo"
-else:
-    model = "gpt-4"
 
 # reset everything
 if clear_button:
-    st.session_state['generated'] = []
-    st.session_state['past'] = []
-    st.session_state['messages'] = [
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["messages"] = [
         {"role": "system", "content": "You are a helpful assistant."}
     ]
-    st.session_state['number_tokens'] = []
-    st.session_state['model_name'] = []
-    st.session_state['cost'] = []
-    st.session_state['total_cost'] = 0.0
-    st.session_state['total_tokens'] = []
-    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    st.session_state["number_tokens"] = []
+    st.session_state["domain_name"] = []
+    counter_placeholder.write(f"Next item ...")
 
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
 
-# generate a response
-def generate_response(prompt):
-    st.session_state['messages'].append({"role": "user", "content": prompt})
+openai.api_key  = os.environ['OPENAI_API_KEY']
+def call_chatgpt(prompt: str) -> str:
+    """
+    Uses the OpenAI API to generate an AI response to a prompt.
 
-    completion = openai.ChatCompletion.create(
-        model=model,
-        messages=st.session_state['messages']
+    Args:
+        prompt: A string representing the prompt to send to the OpenAI API.
+
+    Returns:
+        A string representing the AI's generated response.
+
+    """
+
+    # Use the OpenAI API to generate a response based on the input prompt.
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
     )
-    response = completion.choices[0].message.content
-    st.session_state['messages'].append({"role": "assistant", "content": response})
 
-    # print(st.session_state['messages'])
-    total_tokens = completion.usage.total_tokens
-    prompt_tokens = completion.usage.prompt_tokens
-    completion_tokens = completion.usage.completion_tokens
-    return response, total_tokens, prompt_tokens, completion_tokens
+    # Extract the text from the first (and only) choice in the response output.
+    ans = response.choices[0]["text"]
+
+    # Return the generated AI response.
+    return ans
+
+
+palm_api_key  = os.environ['PALM_API_KEY']
+palm.configure(api_key=palm_api_key)
+
+def call_palm(prompt: str) -> str:
+    completion = palm.generate_text(
+        model="models/text-bison-001",
+        prompt=prompt,
+        temperature=0,
+        max_output_tokens=800,
+    )
+
+    return completion.result
+
+def calculate_cosine_similarity(sentence1: str, sentence2: str) -> float:
+    """
+    Calculate the cosine similarity between two sentences.
+
+    Args:
+        sentence1 (str): The first sentence.
+        sentence2 (str): The second sentence.
+
+    Returns:
+        float: The cosine similarity between the two sentences, represented as a float value between 0 and 1.
+    """
+    # Tokenize the sentences into words
+    words1 = sentence1.lower().split()
+    words2 = sentence2.lower().split()
+
+    # Create a set of unique words from both sentences
+    unique_words = set(words1 + words2)
+
+    # Create a frequency vector for each sentence
+    freq_vector1 = np.array([words1.count(word) for word in unique_words])
+    freq_vector2 = np.array([words2.count(word) for word in unique_words])
+
+    # Calculate the cosine similarity between the frequency vectors
+    similarity = 1 - cosine(freq_vector1, freq_vector2)
+
+    return similarity
+
+
+def levenshtein_distance(s1: str, s2: str) -> float:
+    """
+    Compute the Levenshtein distance between two strings.
+
+    Args:
+        s1 (str): The first string.
+        s2 (str): The second string.
+
+    Returns:
+        float: The Levenshtein distance between the two strings.
+    """
+    m = len(s1)
+    n = len(s2)
+
+    # Create a matrix to store the distances between substrings of s1 and s2
+    d = [[0] * (n + 1) for _ in range(m + 1)]
+
+    # Initialize the first row and column of the matrix
+    for i in range(m + 1):
+        d[i][0] = i
+    for j in range(n + 1):
+        d[0][j] = j
+
+    # Compute the distances between all substrings of s1 and s2
+    for j in range(1, n + 1):
+        for i in range(1, m + 1):
+            if s1[i - 1] == s2[j - 1]:
+                d[i][j] = d[i - 1][j - 1]
+            else:
+                d[i][j] = min(d[i - 1][j], d[i][j - 1], d[i - 1][j - 1]) + 1
+
+    # Return the Levenshtein distance between the two strings
+    return d[m][n] * (-1)
+
+
+def calculate_sts_score(sentence1: str, sentence2: str) -> float:
+    model = SentenceTransformer(
+        "paraphrase-MiniLM-L6-v2"
+    )  # Load a pre-trained STS model
+
+    # Compute sentence embeddings
+    embedding1 = model.encode([sentence1])[0]  # Flatten the embedding array
+    embedding2 = model.encode([sentence2])[0]  # Flatten the embedding array
+
+    # Calculate cosine similarity between the embeddings
+    similarity_score = 1 - cosine(embedding1, embedding2)
+
+    return similarity_score
+
+
+def add_dist_score_column(
+    dataframe: pd.DataFrame, sentence: str, similarity_indicator: str = "cosine"
+) -> pd.DataFrame:
+    if similarity_indicator == "cosine":
+        dataframe["cosine"] = dataframe["questions"].apply(
+            lambda x: calculate_cosine_similarity(x, sentence)
+        )
+    elif similarity_indicator == "levenshtein":
+        dataframe["levenshtein"] = dataframe["questions"].apply(
+            lambda x: calculate_cosine_similarity(x, sentence)
+        )
+    elif similarity_indicator == "sts":
+        dataframe["sts"] = dataframe["questions"].apply(
+            lambda x: calculate_sts_score(x, sentence)
+        )
+    else:
+        dataframe["cosine"] = dataframe["questions"].apply(
+            lambda x: calculate_cosine_similarity(x, sentence)
+        )
+
+    sorted_dataframe = dataframe.sort_values(by=similarity_indicator, ascending=False)
+
+    return sorted_dataframe.iloc[:5, :]
+
+
+def add_dist_score_column(
+    dataframe: pd.DataFrame, sentence: str, similarity_indicator: str = "cosine"
+) -> pd.DataFrame:
+    if similarity_indicator == "cosine":
+        dataframe["cosine"] = dataframe["questions"].apply(
+            lambda x: calculate_cosine_similarity(x, sentence)
+        )
+    elif similarity_indicator == "levenshtein":
+        dataframe["levenshtein"] = dataframe["questions"].apply(
+            lambda x: calculate_cosine_similarity(x, sentence)
+        )
+    elif similarity_indicator == "sts":
+        dataframe["sts"] = dataframe["questions"].apply(
+            lambda x: calculate_sts_score(x, sentence)
+        )
+    else:
+        dataframe["cosine"] = dataframe["questions"].apply(
+            lambda x: calculate_cosine_similarity(x, sentence)
+        )
+
+    sorted_dataframe = dataframe.sort_values(by=similarity_indicator, ascending=False)
+
+    return sorted_dataframe.iloc[:5, :]
+
+
+def convert_to_list_of_dict(df: pd.DataFrame) -> List[Dict[str, str]]:
+    """
+    Reads in a pandas DataFrame and produces a list of dictionaries with two keys each, 'question' and 'answer.'
+
+    Args:
+        df: A pandas DataFrame with columns named 'questions' and 'answers'.
+
+    Returns:
+        A list of dictionaries, with each dictionary containing a 'question' and 'answer' key-value pair.
+    """
+
+    # Initialize an empty list to store the dictionaries
+    result = []
+
+    # Loop through each row of the DataFrame
+    for index, row in df.iterrows():
+        # Create a dictionary with the current question and answer
+        qa_dict_quest = {"role": "user", "content": row["questions"]}
+        qa_dict_ans = {"role": "assistant", "content": row["answers"]}
+
+        # Add the dictionary to the result list
+        result.append(qa_dict_quest)
+        result.append(qa_dict_ans)
+
+    # Return the list of dictionaries
+    return result
+
+
+from PyPDF2 import PdfReader
+
+
+def extract_data(feed):
+    pdf_reader = PdfReader(feed)
+    pages = len(pdf_reader.pages)
+
+    text_list = []
+    for page in range(pages):
+        pdf_page = pdf_reader.pages[page]
+        text = pdf_page.extract_text()
+        text_list.append(text)
+
+    return text
+
+
+if domain_name == "Upload Your Own":
+    st.write("### Upload or select your PDF file")
+    uploaded_file = st.file_uploader("Choose your .pdf file", type="pdf")
+
+    pdf_content = None
+    if uploaded_file is not None:
+        pdf_content = extract_data(uploaded_file)
+        st.success("Page extraction completed.")
 
 
 # container for chat history
@@ -80,31 +295,58 @@ response_container = st.container()
 container = st.container()
 
 with container:
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("You:", key='input', height=100)
-        submit_button = st.form_submit_button(label='Send')
+    with st.form(key="my_form", clear_on_submit=True):
+        user_input = st.text_area("You:", key="input", height=100)
+        submit_button = st.form_submit_button(label="Send")
 
     if submit_button and user_input:
-        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
-        st.session_state['model_name'].append(model_name)
-        st.session_state['total_tokens'].append(total_tokens)
+        if domain_name == "General":
+            processed_user_question = f"""
+                You are an AI assistant for the user.
+                Answer the following question from the user: {user_input}
+            """
+            if model_name == "ChatGPT":
+                output = call_chatgpt(processed_user_question)
+            elif model_name == "Palm":
+                output = call_palm(processed_user_question)
+            else:
+                output = call_chatgpt(processed_user_question)
 
-        # from https://openai.com/pricing#language-models
-        if model_name == "GPT-3.5":
-            cost = total_tokens * 0.002 / 1000
-        else:
-            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+        elif domain_name == "Labcorp 2022 Annual Report":
+            df_screened_by_dist_score = add_dist_score_column(df, user_input, "cosine")
+            qa_pairs = convert_to_list_of_dict(df_screened_by_dist_score)
 
-        st.session_state['cost'].append(cost)
-        st.session_state['total_cost'] += cost
+            processed_user_question = f"""
+                Learn from the context: {qa_pairs}
+                Answer the following question as if you are the AI assistant: {user_input}
+                Produce a text answer that are complete sentences.
+            """
+            if model_name == "ChatGPT":
+                output = call_chatgpt(processed_user_question)
+            elif model_name == "Palm":
+                output = call_palm(processed_user_question)
+            else:
+                output = call_chatgpt(processed_user_question)
+        elif domain_name == "Upload Your Own":
+            processed_user_question = f"""
+                Learn from the context: {pdf_content}
+                Answer the following question as if you are the AI assistant: {user_input}
+                Produce a text answer that are complete sentences.
+            """
+            if model_name == "ChatGPT":
+                output = call_chatgpt(processed_user_question)
+            elif model_name == "Palm":
+                output = call_palm(processed_user_question)
+            else:
+                output = call_chatgpt(processed_user_question)
 
-if st.session_state['generated']:
+        # update session
+        st.session_state["past"].append(user_input)
+        st.session_state["generated"].append(output)
+
+if st.session_state["generated"]:
     with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+        for i in range(len(st.session_state["generated"])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
             message(st.session_state["generated"][i], key=str(i))
-            st.write(
-                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
-            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+            counter_placeholder.write(f"All rights reserved @ Yiqiao Yin")
