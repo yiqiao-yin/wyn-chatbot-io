cd362ad0ee329606dc07265147e0a365f0b3c714
Yiqiao Yin <eagle0504@gmail.com>
2023-06-16 00:31:32 -0400

    test markdown in streamlit chat

--- app.py
+++ app.py
@@ -395,8 +395,6 @@ if st.session_state["generated"]:
             message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
             # message(st.session_state["generated"][i], key=str(i))
             # message(st.markdown(st.session_state["generated"][i]), key=str(i))
-            message(
-                st.code(st.session_state['generated'][i]['data'])
-                # key=f"{i}"
-            )
+            message("", key=f"{i}")
+            st.code(st.session_state['generated'][i]['data'])
             counter_placeholder.write(f"All rights reserved @ Yiqiao Yin")
