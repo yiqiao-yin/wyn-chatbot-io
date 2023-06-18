04f6d0b95e5983dcc6555923b81ec33213b694f6
Yiqiao Yin <eagle0504@gmail.com>
2023-06-18 10:00:43 -0400

    switched to gpt4

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def call_chatgpt(prompt: str) -> str:
 
     # Use the OpenAI API to generate a response based on the input prompt.
     response = openai.Completion.create(
-        model="gpt-4-0314", 
+        model="gpt-4", 
         prompt=prompt,
         temperature=0.5,
         max_tokens=500,
