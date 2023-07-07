67be2242b0c4101079c450c7b0d0724a72396ef1
Yiqiao Yin <eagle0504@gmail.com>
2023-07-06 23:57:31 -0400

    gpt 4 added

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def call_chatgpt(prompt: str) -> str:
 
     # Use the OpenAI API to generate a response based on the input prompt.
     response = openai.Completion.create(
-        model="gpt-4-0613",
+        model="gpt-4",
         prompt=prompt,
         temperature=0.3,
         max_tokens=800,
